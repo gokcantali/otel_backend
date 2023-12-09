f31a350343be0f035da004b4ffc34e1dc076ff0b
Matthias Osswald <matthias@busykoala.io>
2023-12-09 16:40:40 +0100

    Refactor and add 'global store'

--- otel_backend/app.py
+++ otel_backend/app.py
@@ -1,7 +1,17 @@
-from fastapi import FastAPI, Request, HTTPException
-from otel_backend.models import TraceResponse, MetricsResponse, LogsResponse
-from otel_backend.deserializers import deserialize_trace, deserialize_metrics, deserialize_logs
+from fastapi import FastAPI
+from fastapi import HTTPException
+from fastapi import Request
 from otel_backend import logger
+from otel_backend.deserializers import deserialize_logs
+from otel_backend.deserializers import deserialize_metrics
+from otel_backend.deserializers import deserialize_trace
+from otel_backend.models import LogsResponse
+from otel_backend.models import MetricsResponse
+from otel_backend.models import TraceResponse
+from otel_backend.store import STORE
+from otel_backend.store import add_logs_to_store
+from otel_backend.store import add_metrics_to_store
+from otel_backend.store import add_trace_to_store
 
 
 app = FastAPI()
@@ -11,7 +21,8 @@ app = FastAPI()
 async def receive_traces(request: Request) -> TraceResponse:
     try:
         raw_data = await request.body()
-        trace = deserialize_trace(raw_data)
+        trace = await deserialize_trace(raw_data)
+        await add_trace_to_store(trace)
         logger.info(f"Received Trace: {trace}")
         return TraceResponse(status="received")
     except Exception as e:
@@ -23,7 +34,8 @@ async def receive_traces(request: Request) -> TraceResponse:
 async def receive_metrics(request: Request) -> MetricsResponse:
     try:
         raw_data = await request.body()
-        metrics = deserialize_metrics(raw_data)
+        metrics = await deserialize_metrics(raw_data)
+        await add_metrics_to_store(metrics)
         logger.info(f"Received Metrics: {metrics}")
         return MetricsResponse(status="received")
     except Exception as e:
@@ -35,12 +47,28 @@ async def receive_metrics(request: Request) -> MetricsResponse:
 async def receive_logs(request: Request) -> LogsResponse:
     try:
         raw_data = await request.body()
-        logs = deserialize_logs(raw_data)
+        logs = await deserialize_logs(raw_data)
+        await add_logs_to_store(logs)
         logger.info(f"Received Logs: {logs}")
         return LogsResponse(status="received")
     except Exception as e:
         logger.error(f"Error processing request: {e}")
         raise HTTPException(status_code=400, detail=str(e))
+
+
+@app.get("/store/traces")
+async def get_traces() -> list:
+    return STORE['traces']
+
+
+@app.get("/store/metrics")
+async def get_metrics() -> list:
+    return STORE['metrics']
+
+
+@app.get("/store/logs")
+async def get_logs() -> list:
+    return STORE['logs']
 
 
 if __name__ == "__main__":
--- otel_backend/deserializers.py
+++ otel_backend/deserializers.py
@@ -1,14 +1,15 @@
+from google.protobuf.json_format import MessageToDict
+from gzip import GzipFile
+from io import BytesIO
+from opentelemetry.proto.collector.logs.v1 import logs_service_pb2
 from opentelemetry.proto.collector.metrics.v1 import metrics_service_pb2
 from opentelemetry.proto.collector.trace.v1 import trace_service_pb2
-from opentelemetry.proto.collector.logs.v1 import logs_service_pb2
 from otel_backend import logger
-import gzip
-import io
 
 
-def deserialize_trace(data: bytes) -> trace_service_pb2.ExportTraceServiceRequest:
+async def deserialize_trace(data: bytes) -> dict:
     try:
-        with gzip.GzipFile(fileobj=io.BytesIO(data), mode='rb') as f:
+        with GzipFile(fileobj=BytesIO(data), mode='rb') as f:
             decompressed_data = f.read()
     except IOError as e:
         logger.error(f"Error decompressing data: {e}")
@@ -16,15 +17,15 @@ def deserialize_trace(data: bytes) -> trace_service_pb2.ExportTraceServiceReques
     try:
         trace = trace_service_pb2.ExportTraceServiceRequest()
         trace.ParseFromString(decompressed_data)
-        return trace
+        return MessageToDict(trace)
     except Exception as e:
         logger.error(f"Error parsing trace data: {e}")
         raise
 
 
-def deserialize_metrics(data: bytes) -> metrics_service_pb2.ExportMetricsServiceRequest:
+async def deserialize_metrics(data: bytes) -> dict:
     try:
-        with gzip.GzipFile(fileobj=io.BytesIO(data), mode='rb') as f:
+        with GzipFile(fileobj=BytesIO(data), mode='rb') as f:
             decompressed_data = f.read()
     except IOError as e:
         logger.error(f"Error decompressing data: {e}")
@@ -32,15 +33,15 @@ def deserialize_metrics(data: bytes) -> metrics_service_pb2.ExportMetricsService
     try:
         metrics = metrics_service_pb2.ExportMetricsServiceRequest()
         metrics.ParseFromString(decompressed_data)
-        return metrics
+        return MessageToDict(metrics)
     except Exception as e:
         logger.error(f"Error parsing metrics data: {e}")
         raise
 
 
-def deserialize_logs(data: bytes) -> logs_service_pb2.ExportLogsServiceRequest:
+async def deserialize_logs(data: bytes) -> dict:
     try:
-        with gzip.GzipFile(fileobj=io.BytesIO(data), mode='rb') as f:
+        with GzipFile(fileobj=BytesIO(data), mode='rb') as f:
             decompressed_data = f.read()
     except IOError as e:
         logger.error(f"Error decompressing data: {e}")
@@ -48,7 +49,7 @@ def deserialize_logs(data: bytes) -> logs_service_pb2.ExportLogsServiceRequest:
     try:
         logs = logs_service_pb2.ExportLogsServiceRequest()
         logs.ParseFromString(decompressed_data)
-        return logs
+        return MessageToDict(logs)
     except Exception as e:
         logger.error(f"Error parsing logs data: {e}")
         raise
